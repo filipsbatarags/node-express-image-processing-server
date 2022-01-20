66e43a24eadbc90482fd40b7762428da4a2ea0ae
Filips Batarags <filips.batarags@gmail.com>
2022-01-21 00:16:05 +0200

    i

--- api/src/resizeWorker.js
+++ api/src/resizeWorker.js
@@ -4,5 +4,5 @@ gm(workerData.source).resize(100, 100).write(workerData.destination, (error) => 
     if (error) {
         throw error;
     }
-    parentPort.postMesage({ 'resized': true });
+    parentPort.postMessage({ "resized": true });
 });
