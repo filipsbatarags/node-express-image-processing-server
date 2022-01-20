8b04564520e668af649033b807cf5f65c913d679
Filips Batarags <filips.batarags@gmail.com>
2022-01-21 00:52:20 +0200

    zz

--- api/src/imageProcessor.js
+++ api/src/imageProcessor.js
@@ -1,56 +1,73 @@
-const { resolve } = require('path');
 const path = require('path');
 const { Worker, isMainThread } = require('worker_threads');
 
 const pathToResizeWorker = path.resolve(__dirname, 'resizeWorker.js');
-
 const pathToMonochromeWorker = path.resolve(__dirname, 'monochromeWorker.js');
 
 const uploadPathResolver = (filename) => {
     return path.resolve(__dirname, '../uploads', filename);
-}
+};
 
-function imageProcessor(filename) {
+const imageProcessor = (filename) => {
     const sourcePath = uploadPathResolver(filename);
     const resizedDestination = uploadPathResolver('resized-' + filename);
     const monochromeDestination = uploadPathResolver('monochrome-' + filename);
-    const resizeWorkerFinished = false;
-    const monoChromeWorkerFinished = false;
-
+    let resizeWorkerFinished = false;
+    let monochromeWorkerFinished = false;
 
     return new Promise((resolve, reject) => {
         if (isMainThread) {
             try {
-                const resizeWorker = Worker(pathToResizeWorker, { 'workerData': { 'source': sourcePath, 'destination': resizedDestination } })
-                const monochromeWorker = Worker(pathToMonochromeWorker, { 'workerData': { 'source': sourcePath, 'destination': monochromeDestination } })
+                const resizeWorker = new Worker(pathToResizeWorker, {
+                    workerData: {
+                        source: sourcePath,
+                        destination: resizedDestination,
+                    },
+                });
+
+
+                const monochromeWorker = new Worker(pathToMonochromeWorker, {
+                    workerData: {
+                        source: sourcePath,
+                        destination: monochromeDestination,
+                    },
+                });
+
                 resizeWorker.on('message', (message) => {
                     resizeWorkerFinished = true;
-                    if (monoChromeWorkerFinished) {
+                    if (monochromeWorkerFinished) {
                         resolve('resizeWorker finished processing');
                     }
                 });
+
                 resizeWorker.on('error', (error) => {
                     reject(new Error(error.message));
                 });
+
                 resizeWorker.on('exit', (code) => {
                     if (code !== 0) {
                         reject(new Error('Exited with status code ' + code));
                     }
                 });
+
                 monochromeWorker.on('message', (message) => {
-                    monoChromeWorkerFinished = true;
+                    monochromeWorkerFinished = true;
                     if (resizeWorkerFinished) {
                         resolve('monochromeWorker finished processing');
                     }
                 });
-                monochromeWorker.on('exit', (code) => {
-                    if (code !== 0) {
-                        reject(new Error('Exited with status code ' + code))
-                    }
+
+                monochromeWorker.on('error', (error) => {
+                    reject(new Error(error.message));
                 });
 
-            } catch (e) {
-                reject(e);
+                monochromeWorker.on('exit', (code) => {
+                    if (code !== 0) {
+                        reject(new Error('Exited with status code ' + code));
+                    }
+                });
+            } catch (error) {
+                reject(error);
             }
         } else {
             reject(new Error('not on main thread'));
